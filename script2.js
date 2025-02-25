528ede176ebbf4b50aea2eb21620a712f391ff16
kl-keerthan <keerthan2k03mjs@gmail.com>
2025-02-25 18:16:34 +0530

    script2.js

--- script2.js
+++ script2.js
@@ -1,4 +1,3 @@
-//loading
 const loadingElement = document.querySelector('.loading');
 
 setTimeout(() => {
@@ -9,17 +8,11 @@ const loaderElement = document.createElement('div');
 loaderElement.classList.add('loader');
 loadingElement.appendChild(loaderElement);
 
-//loading time
 
-// Get the loading element and the div element
-// const loadingElement = document.querySelector('.loading');
 const divElement = document.querySelector('.emt');
 
-// Simulate a delay (e.g., for loading data)
 setTimeout(() => {
-  // Remove the loading element
   loadingElement.style.display = 'none';
   
-  // Display the div element
   divElement.style.display = 'inline';
 }, 3000);
